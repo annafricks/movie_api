3e7d61d4817ffa35ff732ac40399033567881156
annafricks <annafricks33@gmail.com>
2023-08-14 22:19:21 -0400

    index.html added, edits made

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
 const http = require('http');
-fs = require('fs'),
-url = require('url');
+const fs = require('fs');
+const url = require('url');
 
 http.createServer((request,response) => {
-    let addr = request.url,
-    q = url.parse(addr,true),
+    let url = new URL (request.url, 'http://$request.headers.host/');
+
     filePath = '';
 
-    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Added to log.');
-    }
-});
-
-
-    if (q.pathname.includes('documentation')) {
+    if (url.pathname.includes('documentation')) {
         filePath = (__dirname + '/documentation.html');
     } else {
         filePath = 'index.html';
     }
-    fs.readFile(filePath,(err,data) => {
+
+    fs.appendFile(
+        'log.txt', 
+        'URL: ${request.url} \nTimestamp: ${new Date()} \n \n', (err) => {
+        if (err) {
+            console.log('append error');
+        } else {
+            console.log('msg appended');
+        }
+    });
+
+    fs.readFile(filePath, (err, data) => {
         if (err) {
             throw err;
         }
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
-        response.end();
+        response.end('hello node');
     });
+})
 
-}).listen(8080);
+.listen(8080);
+
 console.log('My test server is running on Port 8080');
+
+//
